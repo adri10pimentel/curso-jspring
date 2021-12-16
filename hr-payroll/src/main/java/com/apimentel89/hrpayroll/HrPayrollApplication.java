45f2f0ed94418f530906f2df809624e92da65e3b
Adriano Pimentel <apimentel@Adrianos-MacBook-Air.local>
2021-12-16 17:06:28 -0300

    adicionando projeto payroll

---
+++ hr-payroll/src/main/java/com/apimentel89/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.apimentel89.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollApplication.class, args);
+	}
+
+}
---
+++ hr-payroll/src/test/java/com/apimentel89/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimentel89.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
